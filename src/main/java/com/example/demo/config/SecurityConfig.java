0077dccc2181c6d759c25cf14cfff8e3e8be22e5
leemj98 <lmj98217@gmail.com>
2024-05-16 10:03:23 +0900

    로그인_필터_구현

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,17 +1,35 @@
 package com.example.demo.config;
 
+import com.example.demo.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // 스프링에서 Configuration 클래스로 관리되기 위해서
 @EnableWebSecurity //Security를 위한 Config이기 때문에
 public class SecurityConfig {
 
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    // LoginFilter에서 AuthenticationManager를 주입받았기 때문에 SecurityConfig에서도 주입하고,
+    // LOginFilter에서 동작할 수 있도록 5번의 new LoginFilter()의 인자로 넣어준다
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     // bCryptPasswordEncoder : 비밀번호 캐시 암호화 메서드
     // 회원가입, 정보 저장, 검증 등을 수행할 때는 항상 비밀번호 캐시 암호화 후 진행함
@@ -47,7 +65,12 @@ public class SecurityConfig {
                 .requestMatchers("/admin").hasRole("ADMIN") // admin 경로는 ADMIN이라는 권한을 가진 사용자만 접근 가능
                 .anyRequest().authenticated()); // 그 외 요청(anyRequest)은 로그인된 사용자만 접근 가능(authenticated)
 
-        // 5. 세션 설정
+        // 5. UsernamePasswordAuthenticationFilter를 커스텀한 LoginFilter 등록
+        // UsernamePasswordAuthenticationFilter를 대채해서 등록하는 것이기 때문에 addFilterAt 선택
+        // addFilterBefore는 지정한 필터 전에 등록하는 것, addFilterAfter는 지정한 필터 후에 등록하는 것
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+        // 6. 세션 설정
         // JWT 방식에서는 세션을 stateless 방식으로 관리하므로 stateless 상태로 설정해주어야 한다
         http.sessionManagement((session)->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
---
+++ src/main/java/com/example/demo/jwt/LoginFilter.java
@@ -0,0 +1,51 @@
+package com.example.demo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+
+        this.authenticationManager = authenticationManager;
+    }
+
+    // 로그인 검증을 진행하기 위한 attemptAuthentication 메서드를 꼭 override시켜줘야 한다.
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        //클라이언트 요청에서 username, password 추출
+        // obtain메서드의 파라미터로 request를 적어주면 request에서 username과 password 추출 가능
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println(username);
+
+        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
+        //꺼낸 username과 password를 Authentication Manager에 전달해 검증 진행하기 위해서 바구니(UsernamePasswordAuthenticationToken)에 담아 던져주는 것 (DTO와 비슷한 개념)
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
+
+        //token에 담은 검증을 위한 AuthenticationManager로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+
+    }
+
+    //로그인 실패시 실행하는 메소드
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+
+    }
+}
