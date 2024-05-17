eb9d9c075468da3f48d66fe86c751e15fdc99fb3
leemj98 <lmj98217@gmail.com>
2024-05-17 10:42:37 +0900

    세션 정보

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -11,6 +17,18 @@ public class MainController {
     @GetMapping("/")
     public String mainP() {
 
-        return "main Controller";
+        // 현재 세션을 통과하는 사용자 이름 가져오기
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // role 값 빼내는 작업은 iterator 반복자로 돌면서 빼내야되기 때문에 조금 복잡하다
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        // username과 role을 responsebody 형태로 리턴
+        return "Main Controller : "+name + role;
     }
 }
